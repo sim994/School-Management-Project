f2afd418455115947320813238d8da9e2fbee778
sim994 <hirenpipaliya418@gmail.com>
2026-03-30 12:41:09 +0530

    Student Data table UI

--- AuthPro/src/componets/Datatable.jsx
+++ AuthPro/src/componets/Datatable.jsx
@@ -1,9 +1,49 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export function Datatable() {
   return (
-    <section className="datatable-section">
-      This Is Data Table Section
+    <section className="data-table-section">
+      <div className="table-wrapper">
+        <div className="table-info-header">
+          <section className="basic-info-section">
+            <h1>Student Records</h1>
+            <p className="total-student-counts">
+              Total 10 students enrolled for Academic Year 2025-26
+            </p>
+          </section>
+          <section className="functionality-section">
+            <input
+              type="text"
+              name="searchStudent"
+              placeholder="Search Student..."
+              className="search-filter"
+            />
+            <select name="All classes" id="class">
+              <option value="All classes">All Classes</option>
+              <option value="Class 1" for="class">
+                class 1
+              </option>
+              <option value="Class 2" for="class">
+                class 2
+              </option>
+              <option value="Class 3" for="class">
+                class 3
+              </option>
+              <option value="Class 4" for="class">
+                class 4
+              </option>
+              <option value="Class 5" for="class">
+                class 5
+              </option>
+            </select>
+          </section>
+          <button className="add-student">
+            <NavLink to="/studentform">Add Student</NavLink>
+          </button>
+        </div>
+        <table className="data-table"></table>
+      </div>
     </section>
   );
 }
