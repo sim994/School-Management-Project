a2de11e2273bf18777eeefd39809933e5ba30cfc
sim994 <hirenpipaliya418@gmail.com>
2026-04-01 12:28:50 +0530

    Filter Section Changes

--- AuthPro/src/componets/Datatable.jsx
+++ AuthPro/src/componets/Datatable.jsx
@@ -6,12 +6,12 @@ export function Datatable() {
     <section className="data-table-section">
       <div className="table-wrapper">
         <div className="table-info-header">
-          <section className="basic-info-section">
+          {/* <section className="basic-info-section">
             <h1>Student Records</h1>
             <p className="total-student-counts">
               Total 10 students enrolled for Academic Year 2025-26
             </p>
-          </section>
+          </section> */}
           <section className="functionality-section">
             <input
               type="text"
@@ -21,28 +21,58 @@ export function Datatable() {
             />
             <select name="All classes" id="class">
               <option value="All classes">All Classes</option>
-              <option value="Class 1" for="class">
-                class 1
-              </option>
-              <option value="Class 2" for="class">
-                class 2
-              </option>
-              <option value="Class 3" for="class">
-                class 3
-              </option>
-              <option value="Class 4" for="class">
-                class 4
-              </option>
-              <option value="Class 5" for="class">
-                class 5
-              </option>
+              <option value="Class 1">class 1</option>
+              <option value="Class 2">class 2</option>
+              <option value="Class 3">class 3</option>
+              <option value="Class 4">class 4</option>
+              <option value="Class 5">class 5</option>
             </select>
+            <select name="All sections" id="section">
+              <option value="All classes">All Sections</option>
+              <option value="A"> A</option>
+              <option value="B"> B</option>
+              <option value="C"> C</option>
+              <option value="D"> D</option>
+            </select>
+            <select name="All statuses" id="status">
+              <option value="All classes">All Statuses</option>
+              <option value="Active">Active</option>
+              <option value="Inactive">Inactive</option>
+              <option value="Suspend">Suspend</option>
+              <option value="On Leave">On Leave</option>
+            </select>
+            <button className="search-data-btn">Search Student</button>
+            <button className="add-data-btn">
+              <NavLink to="/studentform">Add Student</NavLink>
+            </button>
           </section>
-          <button className="add-student">
-            <NavLink to="/studentform">Add Student</NavLink>
-          </button>
         </div>
-        <table className="data-table"></table>
+        <table className="data-table">
+          <thead>
+            <tr>
+              <th>STUDENT</th>
+              <th>CLASS</th>
+              <th>SECTION</th>
+              <th>ROLL NO</th>
+              <th>PARENT CONTACTS</th>
+              <th>STATUS</th>
+              <th>ACTIONS</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <img src="" alt="Image Not Found" />
+              </td>
+              <td>Class 1</td>
+              <td>Section A</td>
+              <td>34</td>
+              <td>+91 99034 38474</td>
+              <td>Active</td>
+              <td className="student-actions"></td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </section>
   );
